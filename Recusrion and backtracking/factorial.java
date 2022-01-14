6f7b4546ccd1e41509920416bcafa76075229b6e
Ankit12bhatt <ankitbhatt640@gmail.com>
2022-01-14 10:45:04 +0530

    firstcommit

---
+++ Recusrion and backtracking/factorial.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=in.nextInt();
+        fact(n);
+    }
+    public static void fact(int n);
+    {
+
+    }
+    
+}
